dbb5b63ec58e51f38942327ef9c1b5f3a7b769a3
ErwinMiranda <70782525+ErwinMiranda@users.noreply.github.com>
2026-02-27 14:54:14 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+  API: {
+  GET: "https://default8c09b36f1cd84669856e1f79d5e305.da.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/35120dd88e5c4d1d8f91533efccad36e/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=v_Ztr6Ff0QjZhC0DSH4-Be7spglEiq-DwYNAIGm67dQ",
+  SAVE: "https://default8c09b36f1cd84669856e1f79d5e305.da.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/e7a10599ed564d0385f44aa39a0da194/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=2yod0uj1vslGE8EIR43aNcEhqBFCJYMveN8hF1yC9N8",
+  DELETE: "https://default8c09b36f1cd84669856e1f79d5e305.da.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/184f8c2b589d4d7287115e61cdbffac5/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=VHK4BtPh-TNlMWywCnPwJbCmGO82MqwZ1M5QFk7-Jjg"
+  }
+};
